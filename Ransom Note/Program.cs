6717d8a53f2b00326f418c37316f9f11f6f2ba85
Akram Boktor <akram.boktor2013@gmail.com>
2022-04-05 07:37:37 +0200

    Ransom Notes problem

---
+++ Ransom Note/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ransom_Note
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.CanConstruct("aa", "ab"));
+        }
+
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            if((ransomNote.Length>=1 && magazine.Length>=1)&&(ransomNote.Length < Math.Pow(10, 5) && magazine.Length < Math.Pow(10, 5)))
+            {
+                foreach (var ch in ransomNote)
+                {
+                    if (!magazine.Contains(ch))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        magazine  = magazine.Remove(magazine.IndexOf(ch), 1);
+                    }
+                    
+                }
+                return true;
+
+            }
+            return false;
+
+        }
+
+        public static string ReversString(string word)
+        {
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
