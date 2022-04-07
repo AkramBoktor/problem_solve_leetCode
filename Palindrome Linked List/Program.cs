7e64c968ffc017923a184a8d89cba437e5229ef2
Akram Boktor <akram.boktor2013@gmail.com>
2022-04-07 08:24:42 +0200

    Plaindrom Linked List

---
+++ Palindrome Linked List/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Palindrome_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode one = new ListNode(1);
+            ListNode two = new ListNode(1);
+            ListNode three = new ListNode(2);
+            ListNode four = new ListNode(1);
+            //ListNode five = new ListNode(3);
+            //ListNode six = new ListNode(2);
+            //ListNode seven = new ListNode(1);
+
+            one.next = two;
+            two.next = three;
+            three.next = four;
+            //four.next = five;
+            //five.next = six;
+            //six.next = seven;
+            Console.WriteLine(Program.IsPalindrome(one));
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public static bool IsPalindrome(ListNode head)
+        {
+            List<int> listItem = new List<int>();
+            ListNode node = head;
+            while (node != null)
+            {
+                listItem.Add(node.val);
+                node = node.next;
+            }
+
+            for (int i = 0, j = listItem.Count-1; i < listItem.Count/2; i++, j--)
+            {
+                if (listItem[i] != listItem[j])
+                    return false;
+                    
+            }
+
+            return true ;
+        }
+    }
+}
