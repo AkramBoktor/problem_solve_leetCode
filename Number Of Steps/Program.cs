f8e3f3e754f81e07b93107dd410649203fcd60bb
Akram Boktor <akram.boktor2013@gmail.com>
2022-04-09 01:42:14 +0200

    Number Of Steps

---
+++ Number Of Steps/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Number_Of_Steps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.NumberOfSteps(14));
+            Console.WriteLine(Program.NumberOfSteps(8));
+            Console.WriteLine(Program.NumberOfSteps(123));
+        }
+
+
+        public static int NumberOfSteps(int num)
+        {
+            int steps= 0;
+            while(num!=0)
+            {
+                num = num % 2 == 0 ? num / 2 : num - 1;
+                steps++;
+            }
+           return steps;
+        }
+    }
+}
