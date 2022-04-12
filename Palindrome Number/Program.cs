0dd5fec9159e36a82ee4677bdc0af60f9ff7bd8b
Akram Boktor <akram.boktor2013@gmail.com>
2022-04-12 07:39:13 +0200

    Plaindrom Number

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.IsPalindrome(-121));
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            string reverseNumber = string.Empty;
+            int lengthOfInteger = x.ToString().Length-1;
+
+            while (lengthOfInteger >= 0)
+            {
+                reverseNumber += x.ToString()[lengthOfInteger];
+                lengthOfInteger--;
+            }
+
+            return reverseNumber == x.ToString() ? true : false;
+            
+        }
+    }
+}
