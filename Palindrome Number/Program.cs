f5458354186fccd20327fe02479a039d6af1fca6
Akram Boktor <akram.boktor2013@gmail.com>
2022-04-12 07:53:56 +0200

    Enhanchment Plaindrom Number

--- Palindrome Number/Program.cs
+++ Palindrome Number/Program.cs
@@ -7,23 +7,32 @@ namespace Palindrome_Number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Program.IsPalindrome(-121));
+            Console.WriteLine(Program.IsPalindrome(123321));
         }
 
         public static bool IsPalindrome(int x)
         {
-            if (x.ToString().Length == 1) return true;
-            string reverseNumber = string.Empty;
-            int lengthOfInteger = x.ToString().Length-1;
+            //if (x.ToString().Length == 1) return true;
+            //string reverseNumber = string.Empty;
+            //int lengthOfInteger = x.ToString().Length-1;
 
-            while (lengthOfInteger >= 0)
-            {
-                reverseNumber += x.ToString()[lengthOfInteger];
-                lengthOfInteger--;
+            //while (lengthOfInteger >= 0)
+            //{
+            //    reverseNumber += x.ToString()[lengthOfInteger];
+            //    lengthOfInteger--;
+            //}
+
+            //return reverseNumber == x.ToString() ? true : false;
+       
+           
+                if (x < 0) return false;
+                String s = x.ToString();
+                for (int i = 0; i < s.Length / 2; i++)
+                {
+                    if (s[i] != s[s.Length - 1 - i]) return false;
+                }
+                return true;
             }
-
-            return reverseNumber == x.ToString() ? true : false;
-            
         }
+
     }
-}
