7ccd29b8c07c57bcab17c3c0daa8f2699479edbb
Akram Boktor <akram.boktor2013@gmail.com>
2022-04-19 21:09:40 +0200

    Implement_strStr

---
+++ Implement strStr/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Implement_strStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.StrStr("aaaaa", "bba"));
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+
+            if (needle == string.Empty) return 0;
+
+            if (haystack.Contains(needle)) return haystack.IndexOf(needle);
+                    
+              return -1;
+
+
+        }
+    }
+}
