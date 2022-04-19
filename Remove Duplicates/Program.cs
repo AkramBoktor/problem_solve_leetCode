2937274812be969cb01f8a342e71b064e3aa9387
Akram Boktor <akram.boktor2013@gmail.com>
2022-04-19 11:19:01 +0200

    Remove Duplicates

---
+++ Remove Duplicates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Remove_Duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.RemoveDuplicates(new int[] { 1, 1, 2 }));
+            Console.WriteLine(Program.RemoveDuplicates(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }));
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            //int countOfNonRepeat = 0;
+
+            //for (int i = 1; i < nums.Length; i++)
+
+            //{
+            //    if(nums[i]!= nums[countOfNonRepeat])
+            //    {
+            //        nums[++countOfNonRepeat] = nums[i];
+            //    }
+            //}
+
+            //return countOfNonRepeat+1;
+            HashSet<int> countNumber = new HashSet<int>();
+
+            foreach (var item in nums)
+            {
+                countNumber.Add(item);
+            }
+            countNumber.CopyTo(nums);
+            return countNumber.Count;
+        }
+    }
+}
