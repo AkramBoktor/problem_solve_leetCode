f4a3f212a093e7d6607cb260c6dc6d859cfce1db
Akram Boktor <akram.boktor2013@gmail.com>
2022-04-24 13:45:36 +0200

    length of the last word

---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Length_of_Last_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.LengthOfLastWord("Hello world"));
+            Console.WriteLine(Program.LengthOfLastWord(" fly me   to   the moon  "));
+            Console.WriteLine(Program.LengthOfLastWord("luffy is still joyboy"));
+            Console.WriteLine(Program.LengthOfLastWord("a"));
+            Console.WriteLine(Program.LengthOfLastWord(" a"));
+            Console.WriteLine(Program.LengthOfLastWord(" "));
+            Console.WriteLine(Program.LengthOfLastWord("    day"));
+
+        }
+
+        public static int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            if (s == string.Empty || s.Length == 0 || s == null) return 0;
+            int length = 0;
+            for (int i = s.Length-1; i >= 0; i--)
+            {
+                if(s[i]!=' ')
+                {
+                    length++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return length;
+            //s = s.Trim();
+            //if (s == string.Empty || s.Length == 0 || s == null) return 0;
+            //string reverseString = string.Empty;
+            //for (int i = s.Length - 1; i >= 0; i--)
+            //{
+            //    reverseString += s[i];
+            //}
+
+            //return reverseString.Contains(" ") ? reverseString.Trim().IndexOf(" ", 0) : reverseString.Length;
+        }
+    }
+}
