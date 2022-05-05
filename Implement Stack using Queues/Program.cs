f1e6973dfd8b2d49641e0378de8e107d8c702fe9
Akram Boktor <akram.boktor2013@gmail.com>
2022-05-05 13:08:17 +0200

    Implement Stack using Queues

---
+++ Implement Stack using Queues/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Implement_Stack_using_Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack myStack = new MyStack();
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Top(); // return 2
+            myStack.Pop(); // return 2
+            Console.WriteLine(myStack.Empty());  // return False
+        }
+
+        public class MyStack
+        {
+            List<int> stackList;
+            public MyStack()
+            {
+                stackList = new List<int>();
+            }
+
+            public void Push(int x)
+            {
+                stackList.Add(x);
+            }
+
+            public int Pop()
+            {
+               int lastItem = stackList[stackList.Count - 1];
+                stackList.Remove(stackList[stackList.Count - 1]);
+                return lastItem;
+            }
+
+            public int Top()
+            {
+               return stackList[stackList.Count - 1];
+            }
+
+            public bool Empty()
+            {
+                return stackList.Count==0;
+            }
+        }
+    }
+}
