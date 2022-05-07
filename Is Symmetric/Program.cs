3ff7bff8e2d1468cf78dd65ffd3a3432a489194a
Akram Boktor <akram.boktor2013@gmail.com>
2022-05-07 19:15:22 +0200

    Is Symmetric Mirror Tree

---
+++ Is Symmetric/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Is_Symmetric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode one = new TreeNode(1);
+            one.right = new TreeNode(2);
+            one.right.left = new TreeNode(3);
+
+            one.left = new TreeNode(2);
+            one.left.right = new TreeNode(3);
+            Console.WriteLine(IsSymmetric(one));
+        }
+
+        public static bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+            return recursive(root.left, root.right);
+        }
+
+        public static bool recursive(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null)
+                return true;
+
+            if (left == null || right == null)     // left=null, right!= null or left!=null, right=null
+                return false;
+
+            if (left.val != right.val)
+                return false;
+
+            return recursive(left.left, right.right) && recursive(left.right, right.left);
+        }
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
---
+++ Remove All Adjacent Duplicates in String II/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Remove_All_Adjacent_Duplicates_in_String_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", RemoveDuplicates("pbbcggttciiippooaais", 2)));
+        }
+
+        public static string RemoveDuplicates(string s, int k)
+        {
+            StringBuilder sNew = new StringBuilder();
+            sNew.Append(s);
+            HashSet<int> RemoveDuplicates = new HashSet<int>();
+            int i = 0, j  , countofRepeat ;
+            int coubtOfCharacters = s.Length;
+            while (coubtOfCharacters != 0)
+            {
+                j = i + 1;
+                countofRepeat = 1;
+                while (j<= sNew.Length - 1)
+                {
+                    if (sNew[i] == sNew[j])
+                    {
+                        RemoveDuplicates.Add(i);
+                        RemoveDuplicates.Add(j);
+                        countofRepeat++;
+                    }
+                    j++;
+                }
+                if (countofRepeat >= k)
+                {
+                    countofRepeat = k;
+                    foreach (var item in RemoveDuplicates)
+                    {
+                            if (countofRepeat == 0) break;
+                           sNew[item] = '-';
+                             countofRepeat--;
+                    }
+                    i = 0;
+                }
+                else
+                {
+                    i++;
+                }
+                sNew = sNew.Replace("-", "");
+                coubtOfCharacters--;
+                RemoveDuplicates.Clear();
+            }
+
+
+            return sNew.ToString();
+        }
+
+        
+    }
+}
+
+     
