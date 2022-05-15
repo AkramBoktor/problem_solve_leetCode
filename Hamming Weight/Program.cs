1b2da75cceb0f78367c668d4ff9fe52e911a194f
Akram Boktor <akram.boktor2013@gmail.com>
2022-05-15 11:55:03 +0200

    Hamming Weight

---
+++ Hamming Weight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Hamming_Weight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.HammingWeight(00000000000000000000000000001011));
+            Console.WriteLine(Program.HammingWeight(00000000000000000000000000001011));
+           
+        }
+
+        public static int HammingWeight(uint n)
+        {
+            int count = 0;
+
+            while (n!=0)
+            {
+                n &= (n - 1);
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
