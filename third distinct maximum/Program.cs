547fc84c2a14b891d5fd9dc47f219d93cb1de91a
Akram Boktor <akram.boktor2013@gmail.com>
2022-05-25 21:15:18 +0200

    Third Distinct Maximum

---
+++ third distinct maximum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+
+namespace third_distinct_maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Program.ThirdMax(new int[] { 3, 2, 1 }));
+            Console.WriteLine(Program.ThirdMax(new int[] { 1, 2 }));
+            Console.WriteLine(Program.ThirdMax(new int[] { 1, 1 ,2 }));
+
+            //Console.WriteLine(Program.ThirdMax(new int[] { 2, 2, 3, 1 }));
+            //Console.WriteLine(Program.ThirdMax(new int[] { 3, 3, 4, 3, 4, 3, 0, 3, 3 }));
+        }
+
+
+        public static int ThirdMax(int[] nums)
+        {
+           var numDistinct =  nums.Distinct().OrderByDescending(num => num).ToArray();
+            if (numDistinct.Length <= 2) return numDistinct.Max();
+            return numDistinct.Select((s, i) => new { i, s })
+         .Where(t => t.i == 2)
+         .Select(t => t.s)
+         .FirstOrDefault();
+        }
+
+    }
+}
