2549e5d15ae2e1a85fec2417ffda0220b8e73ec0
Akram Boktor <akram.boktor2013@gmail.com>
2022-05-31 06:54:21 +0200

    Nums of Jewels in stones

---
+++ Jewels and Stones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Jewels_and_Stones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.NumJewelsInStones("Aa", "AaZZA"));
+            Console.WriteLine(Program.NumJewelsInStones("aA", "aAAbbbb"));
+            Console.WriteLine(Program.NumJewelsInStones("z", "ZZ"));
+        }
+
+
+        public static int NumJewelsInStones(string jewels, string stones)
+        {
+            if (string.IsNullOrEmpty(jewels)) return 0;
+            int countOfJewels = 0;
+            for (int i = 0; i < jewels.Length; i++)
+            {
+               
+                countOfJewels += stones.Where(c => c == jewels[i]).Count();
+            }
+
+            return countOfJewels;
+        }
+    }
+}
