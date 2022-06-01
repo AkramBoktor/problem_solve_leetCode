b284824f76901bb9e60462d5ac91543ed5ca1214
Akram Boktor <akram.boktor2013@gmail.com>
2022-06-01 10:00:27 +0200

    Rotat string

---
+++ Rotate String/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Rotate_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.RotateString("abcde", "cdeab"));
+            Console.WriteLine(Program.RotateString("abcde", "abced"));
+            Console.WriteLine(Program.RotateString("abcde", "bcdea"));
+        }
+
+
+        public static bool RotateString(string s, string goal)
+        {
+            char [] arrRotated = new char[s.Length];
+            arrRotated = s.ToCharArray();
+            int count = 0;
+            while(count < arrRotated.Length)
+            {
+              char FirstCahracter = arrRotated[0];
+
+                for (int j = 0; j < arrRotated.Length; j++)
+                {
+                    arrRotated[j] = j == arrRotated.Length - 1 ?  FirstCahracter : arrRotated[j + 1];
+
+                    
+                }
+                if(new string(arrRotated) == goal)
+                {
+                    return true;
+                }
+                count++;
+            }
+
+            return false;
+        }
+    }
+}
+
+
+//        return (s + s).Contains(goal) && s.Length == goal.Length;
