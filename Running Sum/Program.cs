6fe2f7346ecb331b4f845c3af630b8a4f049fc56
Akram Boktor <akram.boktor2013@gmail.com>
2022-06-02 06:19:24 +0200

    Running Sum

---
+++ Running Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Running_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(',', Program.RunningSum(new int[] { 1, 2, 3, 4 }))); //1,3,6,10
+            Console.WriteLine(string.Join(',', Program.RunningSum(new int[] { 1, 1, 1, 1, 1 }))); //1,2,3,4,5
+            Console.WriteLine(string.Join(',', Program.RunningSum(new int[] { 3, 1, 2, 10, 1 }))); //3,4,6,16,17
+        }
+
+
+        public static int[] RunningSum(int[] nums)
+        {
+            int[] arrSum = new int[nums.Length];
+            
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    arrSum[i] = arrSum[i] + nums[j];
+                }
+            }
+
+            return arrSum;
+        }
+    }
+}
+
+
+/*LINQ
+ *     public int[] RunningSum(int[] nums) {
+        int total = 0;
+        return nums.Select(x => total += x).ToArray();
+    }*/
+
+/*
+ for (int i = 1; i < nums.Length; ++i) 
+	{
+		nums[i] += nums[i - 1]; 
+	}
+	return nums;
+ 
+ */
---
+++ Unique Morse Code Words/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique_Morse_Code_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int UniqueMorseRepresentations(string[] words)
+        {
+            //int 
+            //Dictionary<string, string> moruseCode = new Dictionary<string, string>();
+            //List<String> hashedListOfString = new List<string>();
+            //moruseCode.Add("","")
+            return 0;
+        }
+    }
+}
+
+
+/*
+ Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+ 
+ */
