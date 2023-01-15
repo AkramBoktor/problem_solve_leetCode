4de7915bb2501130cbeee884527b53bd66e0b0c1
Akram Boktor <akram.boktor2013@gmail.com>
2023-01-15 21:08:28 +0200

    second solution

--- (Medium)Longest Palindromic Substring/Program.cs
+++ (Medium)Longest Palindromic Substring/Program.cs
@@ -73,5 +73,34 @@ namespace _Medium_Longest_Palindromic_Substring
 
             return PlaindromList.OrderByDescending(s=>s.Length).FirstOrDefault() !=null ? PlaindromList.OrderByDescending(s => s.Length).FirstOrDefault() : new string( s.ToCharArray(0,1));
         }
+
+
+        #region 
+/*
+ if (s == null || s.Length == 0) return "";
+
+int maxStart = 0, maxEnd = 0;
+
+for(int i=0; i<s.Length; i++)
+{
+    int start = i, end = i;
+
+    while( (start > 0 && end < s.Length - 1 && s[start-1] == s[end+1])) { start--; end++; }
+
+    if (end - start > maxEnd - maxStart) { maxStart = start; maxEnd = end; }
+
+    if (i<s.Length-1 && s[i] == s[i+1])
+    {
+        start = i; end = i+1;
+
+        while( (start > 0 && end < s.Length - 1 && s[start-1] == s[end+1])) { start--; end++; }
+
+        if (end - start > maxEnd - maxStart) { maxStart = start; maxEnd = end; }
     }
+}
+
+return s.Substring(maxStart, maxEnd-maxStart+1);
+*/
+#endregion
+}
 }
