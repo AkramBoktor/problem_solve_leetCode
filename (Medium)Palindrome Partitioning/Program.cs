fd602f0aaf5ebd2f2bd13dd62941513353f6da05
Akram Boktor <akram.boktor2013@gmail.com>
2023-01-22 20:23:16 +0200

    Move Zeroes  solve problem

---
+++ (Medium)Palindrome Partitioning/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _Medium_Palindrome_Partitioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Partition("aab"));
+        }
+
+
+
+        public static IList<IList<string>> Partition(string s)
+        {
+            List<string> partitionList = new List<string>();
+
+           var strTocharArray = s.ToCharArray();
+
+            string strBind = string.Empty;
+            for (int i = 0; i < strTocharArray.Length; i++)
+            {
+                strBind += strTocharArray[i];
+                partitionList.Add(strBind);
+                for (int j = i+1; j < strTocharArray.Length; j++)
+                {
+                    if (strTocharArray[j] == strTocharArray[i])
+                    {
+                        strBind += strTocharArray[j];
+                        partitionList.Add(strBind);
+                    }
+                    else
+                    {
+                        strBind += strTocharArray[j];
+                        var plaindromCharArray = strBind.ToCharArray();
+                        Array.Reverse(plaindromCharArray);
+                        if (s == new string(plaindromCharArray))
+                        {
+                            partitionList.Add(strBind);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ MoveZeroes/Program.cs
@@ -0,0 +1,55 @@
+﻿
+/*Given an integer array nums, 
+    move all 0's to the end of it while maintaining the relative order 
+    of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0, 1, 0, 3, 12]
+Output:[1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output:[0]
+*/
+using System;
+
+namespace MoveZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.MoveZeroes(new int[] { 0 , 0 , 1 , 1});
+        }
+
+        public static  void MoveZeroes(int[] nums)
+        {
+            if (nums.Length == 0 || nums == null) return;
+            if (nums.Length == 1) Console.WriteLine(String.Join(',', nums));
+
+            int length = nums.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    for (int j = i; j < length - 1; j++)
+                    {
+                        nums[j] = nums[j + 1];
+                    }
+
+                    nums[length-1] = 0;
+                    i--;
+                    length--;
+                }
+            }
+
+            Console.WriteLine(String.Join(',', nums));
+        }
+
+    }
+}
