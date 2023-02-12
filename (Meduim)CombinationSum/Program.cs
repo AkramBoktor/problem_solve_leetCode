d9ee49ae0b5e9e95338c9c75be1e8b43f3b35ee7
Akram Boktor <akram.boktor2013@gmail.com>
2023-02-12 20:48:56 +0200

    combination part 1

---
+++ (Meduim)CombinationSum/Program.cs
@@ -0,0 +1,86 @@
+﻿/*
+ Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+frequency
+ of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+*/
+
+/*
+ Follow the below steps to implement the idea:
+
+Sort the array arr[] and remove all the duplicates from the arr[] then create a temporary vector r. to store every combination and a vector of vector res.
+Recursively follow: 
+If at any time sub-problem sum == 0 then add that array to the res (vector of vectors).
+Run a while loop till the sum – arr[I] is not negative and i is less than arr.size().
+Push arr[i] in r and recursively call for i and sum-arr[i] ,then increment i by 1.
+pop r[i] from back and backtrack. 
+Below is the implementation of the above approach.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _Meduim_CombinationSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            List<List<int>> result = new List<List<int>>();
+            List<int> temp = new List<int>();
+
+            HashSet<int> setOfArrays = new HashSet<int>(candidates.ToList());
+            Array.Clear(candidates, 0, candidates.Length);
+            candidates.ToList().AddRange(setOfArrays);
+            Array.Sort(candidates);
+
+            findCombination(result, candidates, target ,temp);
+            return null;
+        }
+
+        private static void findCombination(List<List<int>> result, int[] candidates, int target ,List<int> temp)
+        {
+            if( target == 0)
+            {
+                result.Add(new List<int>(temp));
+
+                return;
+            }
+
+            for (int i = 0; i < candidates.Length; i++)
+            {
+                if(target - candidates[i] >= 0)
+                {
+
+                }
+            }
+        }
+    }
+}
